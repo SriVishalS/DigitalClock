6d374d9ad1708e170bad50f6ba0e01e7dfaf73bf
SriVishalS <vishals221202@gmail.com>
2022-08-31 18:20:45 +0530

    some Changes

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-let clock = () => {
-    let date = new Date();
-    let hrs = date.getHours();
-    let mins = date.getMinutes();
-    let secs = date.getSeconds();
-    let period = "AM";
-    if (hrs == 0) {
-     hrs = 12;
-    } else if (hrs >= 12) { 
-    } if (hrs > 12) {
-      hrs = hrs - 12;
-      period = "PM";
-    }
-    hrs = hrs < 10 ? "0" + hrs : hrs;
-    mins = mins < 10 ? "0" + mins : mins;
-    secs = secs < 10 ? "0" + secs : secs;
-  
-    let time = `${hrs}:${mins}:${secs}:${period}`;
-    document.getElementById("clock").innerText = time;
-    setTimeout(clock, 1000);
-  };
-  
-  clock();
+displayCurrentTime = () => {
+      let currDate = new Date();
+      let hours = currDate.getHours();
+      let minutes = currDate.getMinutes();
+      let seconds = currDate.getSeconds();
+      session = "AM"
+      if (hours > 12) {
+            hours = hours - 12;
+            session = "PM";
+      }
+      if (hours == 0) {
+            hours = 12;
+      }
+      hours = hours < 10 ? `0${hours}` : `${hours}`;
+      minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+      seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+      let time = `${hours}:${minutes}:${seconds} ${session}`;
+      document.getElementById("timeDisp").innerHTML = `${time}`;
+}
+displayCurrentDate = () => {
+      let currDate = new Date();
+      const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      const daysInAWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+      let date = currDate.getDate();
+      let day = daysInAWeek[currDate.getDay()];
+      let month = monthNames[currDate.getMonth()];
+      let year = currDate.getFullYear();
+      let dateDisplay = `${date} ${day} ${month} ${year}`;
+      document.getElementById("dateDisp").innerHTML = `${dateDisplay}`;
+}
+let liveTime = setInterval(function () {
+      displayCurrentTime();
+      displayCurrentDate();
+}, 1000)
